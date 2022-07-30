8fc22e23ddabaa9582e52c9b414f2bccd2b3aca2
Prahas <saiprahas@outlook.com>
2022-07-30 23:36:21 +0530

    Added copy and paste mappings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -2,6 +2,8 @@
 vim.cmd('nnoremap <C-s> :w<CR>') -- save
 vim.cmd('nnoremap <C-x> :x<CR>') -- save and quit
 vim.cmd('nnoremap <C-q> :q!<CR>') -- quit without saving
+vim.cmd('vnoremap <C-c> "+y<CR>')
+vim.cmd('vnoremap <C-v> "+p<CR>')
 
 -- nvim-tree
 vim.cmd('nnoremap <C-n> :NvimTreeToggle<CR>')
--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   auto_install = false,
-  ensure_installed = { "c", "rust", "cpp", "lua" },
+  ensure_installed = { "c", "cpp", "rust", "lua" },
   sync_install = false,
   auto_install = false,
   highlight = {
