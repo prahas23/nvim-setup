dae63e6d892bc4e756f4dc42b4f7ae41cfdd131d
Prahas <saiprahas@outlook.com>
2022-09-17 23:35:25 +0530

    cmp vsnip doesn't work. so commented

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function()
   use 'kyazdani42/nvim-tree.lua'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-vsnip'
+  -- use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-buffer'
   use 'simrat39/rust-tools.nvim'
